b8f211db64418ae12c6ce04fb0b3958066ff1f3e
TorrezMN <torrez.mn@gmail.com>
2022-04-26 22:14:10 -0400

    :sparkles: ADDED - Basic ETL
    Added basic ETL setup to load data in the app.

--- etl/src/hell_world.py
+++ etl/src/hell_world.py
@@ -1,6 +1,54 @@
+import os
+import subprocess
+from os.path import exists
+from pathlib import Path
 from time import sleep
 
-from dagster import job, op
+import pandas as pd
+from dagster import file_relative_path, graph, job, op, pipeline
+from dagster_shell import create_shell_command_op, create_shell_script_op
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+@op
+def print_row(row):
+    print('ROW ----------> ', row)
+
+@op
+def read_csv_file():
+    csv_file_dir = 'csv_data/covid_py.csv'
+    df = pd.read_csv(csv_file_dir, delimiter=';', chunksize=100)
+    print('==============================================')
+    print('BASE DIR FROM READ_CSV FILE!', BASE_DIR)
+    print('==============================================')
+    for chunk in df:
+        chunk.apply(print_row)
+    print('==============================================')
+
+
+
+
+@graph
+def download_csv_dataset():
+    a = create_shell_script_op(file_relative_path(__file__, "shell_scripts/hello_shell.sh"), name="a")
+    file_exists = exists(os.path.join(BASE_DIR, 'src', 'csv_data', 'covid_py.csv'))
+
+
+    if(file_exists):
+        print('YA SE DESCARGO EL ARCHIVO!')
+    else:
+        a()
+
+
+    print('==============================================')
+    read_csv_file()
+    print('==============================================')
+
+
+
+
 
 
 @op
@@ -19,3 +67,4 @@ def hello(name: str):
 def hello_dagster():
     sleep(10)
     hello(get_name())
+    download_csv_dataset()
---
+++ etl/src/shell_scripts/hello_shell.sh
@@ -0,0 +1,7 @@
+
+
+
+echo "CSV DATA DOWNLOADING STARTED NOW!"
+echo "========================================================================="
+wget --no-check-certificate -O csv_data/covid_py.csv "https://www.mspbs.gov.py/xweb/vacunados.csv"
+echo "========================================================================="
