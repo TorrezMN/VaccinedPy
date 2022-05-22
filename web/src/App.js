c6cfe99006999de5b0e6000923bfb2cef490f855
TorrezMN <torrez.mn@gmail.com>
2022-05-22 17:51:30 -0400

    :bricks: CHANGED - web app
    Changed web app from dash app to reactjs app.

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+	document.title= 'Vaccined Py'
+  return (
+    <div className="App">
+	  <center>
+		<h1>THIS IS WEB</h1>
+	  </center>
+    </div>
+  );
+}
+
+export default App;
