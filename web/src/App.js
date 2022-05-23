70796c920b09bc572828761b4690f1c78bf32a20
TorrezMN <torrez.mn@gmail.com>
2022-05-22 21:09:41 -0400

    :lipstick: STYLES WEB
    Added landing page for web app. With some styles too.

--- web/src/App.js
+++ web/src/App.js
@@ -4,9 +4,29 @@ function App() {
 	document.title= 'Vaccined Py'
   return (
     <div className="App">
-	  <center>
-		<h1>THIS IS WEB</h1>
-	  </center>
+	  <div className='searchBar'>
+		<div className='searchTitle'>
+		  <center>
+			<h3>Search by</h3>
+		  </center>
+		</div>
+		<div className='searchBoxes'>
+			<div className='search_option'>
+				<label for="search_name">Name:</label>
+				<input type="text" id="search_name" name="search_name"/>
+			</div>
+			<div className='search_option'>
+				<label for="search_date">Date:</label>
+				<input type="datetime-local" name="search_date" id="search_date"/>
+			</div>
+			<div className='search_option'>
+				<label for="search_quant">Quantity:</label>
+				<input type="range" name="search_quant" id="price" min="50000" max="500000" step="100" value="250000"/>
+			</div>
+		</div>
+
+
+	  </div>
     </div>
   );
 }
