686a81a4c65b1d1c54d748433222e3b28c6c899c
TorrezMN <torrez.mn@gmail.com>
2022-06-06 21:30:16 -0400

    :zap: IMPROVE ETL
    Added schedule configuration for etl daemon.

--- etl/src/repositories/vacc_repositories.py
+++ etl/src/repositories/vacc_repositories.py
@@ -6,9 +6,10 @@ from dagster import repository
 #  JOBS
 from jobs.vacc_jobs import download_csv_dataset
 #  SCHEDULES
+from schedules.vacc_schedules import every_five_minutes
 
 
 @repository
 def vacc_repo():
     """Vaccined repository."""
-    return [download_csv_dataset]
+    return [download_csv_dataset, every_five_minutes]
--- etl/src/schedules/vacc_schedules.py
+++ etl/src/schedules/vacc_schedules.py
@@ -2,16 +2,17 @@
 
 from dagster import schedule
 
+from ops.vacc_ops import download_csv
 #  JOBS
-from jobs.vacc_jobs import test_job
+from jobs.vacc_jobs import download_csv_dataset
 
 
-#  @schedule(
-    #  cron_schedule="*/5 * * * *",
-    #  job=test_job,
-    #  execution_timezone="Europe/Stockholm",
-#  )
-#  def every_five_minutes(context):
-    #  """THIS WILL RUN EVERY FIVE MINUTES."""
-    #  date = context.scheduled_execution_time.strftime("%Y-%m-%d")
-    #  return {"ops": {"download_csv": {"config": {"date": date}}}}
+@schedule(
+    cron_schedule="0 * * * *",
+    job=download_csv_dataset,
+    execution_timezone="America/Asuncion",
+)
+def every_five_minutes(context):
+    """THIS WILL RUN EVERY FIVE MINUTES."""
+    date = context.scheduled_execution_time.strftime("%Y-%m-%d")
+    return {"ops": {"download_csv": {"config": {"date": date}}}}
